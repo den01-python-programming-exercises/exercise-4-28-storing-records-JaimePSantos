c32178d0034d85070ebb3ae92834ef86ed4d7055
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-27 20:04:34 +0000

    another one

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,10 +1,10 @@
 import pytest
 import os
+from src.exercise import read_records_from_file
 
 def test_exercise(capsys):
-    # os.chdir('src')
-    from src.exercise import read_records_from_file #had to change this import
-    file = "src\data.txt"
+    os.chdir('src')
+     #had to change this import
+    file = "data.txt"
     records = read_records_from_file(file)
-
     assert records == ["lily","anton","levi","amy"]
